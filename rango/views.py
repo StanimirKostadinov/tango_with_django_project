7b352fcce12e014226e1620bcf14710ff1c7d3eb
StanimirKostadinov <2264064k@student.gla.ac.uk>
2019-01-25 18:08:43 +0000

    linking views

--- rango/views.py
+++ rango/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    message = ''' Rango says hey tehre partner!
-	<br/> %s <a href='/rango/about/'>About</a>
-	'''
-    return HttpResponse(message )
+    message = "Rango says hey tehre partner!"
+	
+	html =" <br/>  <a href='/rango/about/'>About</a>"
+	
+    return HttpResponse("%s" + "/n"  " %s " % message, html )
 
 def about(request):
-    message = '''
-	Rango says here is the about page.	
-	<h2><a href='/rango/'>Index</a><h2> '''
-    return HttpResponse(message )
+    message =  "Rango says here is the about page."	
+	html = "<h2><a href='/rango/'>Index</a><h2> '''
+    return HttpResponse("%s" + "/n"  " %s " % message, html)
