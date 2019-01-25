71560eec049c9a4f5bb8f017bf6662eb564aea65
StanimirKostadinov <2264064k@student.gla.ac.uk>
2019-01-25 18:29:49 +0000

    fix_8

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 def index(request):
     message = "Rango says hey tehre partner!"
     html = "<br/> <a href='/rango/about/'>About</a>"
-    return HttpResponse("%s  %s " % message, html)
+    return HttpResponse("%s  %s " % (message, html))
 
 def about(request):
     message =  "Rango says here is the about page."
     html = " <a href='/rango/'>Index</a> "
-    return HttpResponse("%s  %s " % message, html)
+    return HttpResponse("%s  %s " % (message, html))
