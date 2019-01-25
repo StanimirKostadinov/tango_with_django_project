33046ffde21fd57a42c2d8761f3afd4614b860a9
StanimirKostadinov <2264064k@student.gla.ac.uk>
2019-01-25 18:04:26 +0000

    HTWD chapter 3.6 linking views final

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    message = "Rango says hey tehre partner!"
-    return HttpResponse("<br/> %s <a href='/rango/about/'>About</a>"  % message )
+    message = ''' Rango says hey tehre partner!
+	<br/> %s <a href='/rango/about/'>About</a>
+	'''
+    return HttpResponse(message )
 
 def about(request):
-    message = 'Rango says here is the about page.'
-    return HttpResponse(" %s <a href='/rango/'>Index</a>" % message)
+    message = '''
+	Rango says here is the about page.	
+	<h2><a href='/rango/'>Index</a><h2> '''
+    return HttpResponse(message )
