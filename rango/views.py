629a39c1003498826f9e00544950cbd26f6c432b
StanimirKostadinov <2264064k@student.gla.ac.uk>
2019-01-25 18:26:00 +0000

    Chapter 3.6 fix_6

--- rango/views.py
+++ rango/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     message = "Rango says hey tehre partner!"
-	html ="<br/> <a href='/rango/about/'>About</a>"
-	return HttpResponse("%s" + "/n" + " %s " % (message, html))
+    html = "<br/> <a href='/rango/about/'>About</a>"
+    return HttpResponse("%s  %s " % (message, html))
 
 def about(request):
-    message =  "Rango says here is the about page."	
-	html = " <a href='/rango/'>Index</a> "
-	return HttpResponse("%s" + "/n" + " %s " % (message, html))
+    message =  "Rango says here is the about page."
+    html = " <a href='/rango/'>Index</a> "
+    return HttpResponse("%s  %s " % (message, html))
