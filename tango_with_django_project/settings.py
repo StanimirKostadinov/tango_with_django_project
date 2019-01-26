8178597d9755f1587a6507e00058c439e3e6a48c
StanimirKostadinov <2264064k@student.gla.ac.uk>
2019-01-26 12:53:03 +0000

    Printing the abs path to the dir of __file__

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -101,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+print (__file__)
+print (os.path.dirname(__file__))
+print (os.path.dirname(os.path.dirname(__file__)))
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
